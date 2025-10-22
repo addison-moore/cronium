c6ec6548a810c59ebc00e4b7f7c1f6a9c4ea6171
Addison Moore <addisondrewmoore@gmail.com>
2025-10-22 01:15:02 -0700

    removed unused UI components

--- packages/ui/src/components/monaco-editor-lazy.tsx
+++ packages/ui/src/components/monaco-editor-lazy.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 import type { ComponentType } from "react";
-import { CodeEditorSkeleton } from "./loading-skeletons";
+import { Skeleton } from "./skeleton";
 import type { MonacoEditorProps } from "./monaco-editor";
 
 // Explicitly type the dynamic import to avoid type inference issues
@@ -11,7 +11,7 @@ export const MonacoEditor: ComponentType<MonacoEditorProps> = dynamic(
     import("./monaco-editor").then((mod) => ({ default: mod.MonacoEditor })),
   {
     ssr: false,
-    loading: () => <CodeEditorSkeleton />,
+    loading: () => <Skeleton className="h-[400px] w-full" />,
   },
 ) as ComponentType<MonacoEditorProps>;
 
